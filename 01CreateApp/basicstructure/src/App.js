c29046f2354ab9df925d432f2a6ca3eb6f1211b9
Sanjay Choudhary <choudharysanjayjaat091@gmail.com>
2026-05-15 10:05:29 +0530

    fixed submodule issue

---
+++ 01CreateApp/basicstructure/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+         <h1>Hello React with Create-React-App</h1>
+         
+         <h1>Hello Sanjay you are updating at this time</h1>
+        <H1>Hello React</H1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
